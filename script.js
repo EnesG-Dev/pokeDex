325e2fd5c04df7ee04ab66824d8e3d6ab218a4b7
Enes Geldi <enes@mbp-von-enes.localdomain>
2024-08-09 22:08:22 +0200

    clean conde and render background color

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const ALLPOKEMON = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
 const POKEAPI = 'https://pokeapi.co/api/v2/pokemon/' // ID/
+const POKE_SPECIES = 'https://pokeapi.co/api/v2/pokemon-species/' // ID/
 const POKE_IMG = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/' // 150.svg
 
 let idCounter = 1;
@@ -11,33 +12,47 @@ async function renderPreviews() {
         let id = idCounter;
         let pokeUrl = POKEAPI + id + '/';
         let pokemon = await fetchDataJson(pokeUrl);
-        document.getElementById('previewContain').innerHTML += previewTemp(pokemon);
-        types(pokemon);
+        document.getElementById('previewContain').innerHTML += getPreviewTemp(pokemon);
+        
+        renderTypes(pokemon);
+        setBackgroundColor(pokemon);
     }
     toId = toId+20;
 }
+
+// async function pokeSpecies(id) {
+//     let speciesUrl = POKE_SPECIES + id + '/';
+//     let getSpeciesApi = await fetchDataJson(speciesUrl);
+//     let pokeColor = getSpeciesApi.color.name;
+//     return pokeColor;
+// }
 
 function getPokeImg(id) {
     let imgUrl = POKE_IMG + id + '.svg';
     return imgUrl;
 }
 
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function types(pokemon) {
+function renderTypes(pokemon) {
     let typesArr = pokemon.types;
+
     typesArr.forEach(slot => {
         let type = slot.type.name;
-        
-        let typeImg = `Pokemon_Type_Icon_${capitalizeFirstLetter(type)}.svg`;
+        let typeImgName = `Pokemon_Type_Icon_${capitalizeFirstLetter(type)}.svg`;
     
-    document.getElementById(`pokeType_${pokemon.id}`).innerHTML += getTypeTemp(typeImg);
+        document.getElementById(`pokeType_${pokemon.id}`).innerHTML += getTypeTemp(typeImgName);
     });
 }
 
+function setBackgroundColor(pokemon) {
+    let firstType = pokemon.types[0].type.name;
+    let typeColor = capitalizeFirstLetter(firstType);
+
+    document.getElementById(`backgroundColor_${pokemon.id}`).style = `background-color: ${backgroundColors[typeColor]}`;
+}
 
 
 async function fetchDataJson(api) {
@@ -54,28 +69,3 @@ function init() {
     renderPreviews();
     console.log('go!');
 }
-
-
-
-
-function previewTemp(pokemon) {
-    return /*html*/`
-        <div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-2">
-            <div class="square">
-                <div class="square-content pokemon-container poke-box-shadow rounded">
-                    <span class="pokemon-id"># ${pokemon.id}</span>
-                    <img src="${getPokeImg(pokemon.id)}" class="rounded d-block poke-filter zoom">
-                    <div class="pokemon-type" id="pokeType_${pokemon.id}"></div>
-                    <span class="pokemon-name">${pokemon.name}</span>
-                </div>
-            </div>
-        </div>
-    `
-}
-
-function getTypeTemp(src) {
-    return /*html*/`
-        <img src="./assets/img/type_icons/${src}">
-    `
-}
-
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,4 +1,20 @@
-function getHtmlTemplate(note) {
-    return `<p>${note}</p>`;
+function getPreviewTemp(pokemon) {
+    return /*html*/`
+        <div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-2">
+            <div class="square">
+                <div id="backgroundColor_${pokemon.id}" class="square-content pokemon-container poke-box-shadow rounded">
+                    <span class="pokemon-id"># ${pokemon.id}</span>
+                    <img src="${getPokeImg(pokemon.id)}" class="rounded d-block poke-filter zoom">
+                    <div class="pokemon-type" id="pokeType_${pokemon.id}"></div>
+                    <span class="pokemon-name">${pokemon.name}</span>
+                </div>
+            </div>
+        </div>
+    `
 }
 
+function getTypeTemp(src) {
+    return /*html*/`
+        <img src="./assets/icons/type_icons/${src}">
+    `
+}
