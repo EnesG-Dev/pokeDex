b711834af249677b408f8e1be7a737026fb89df3
Enes Geldi <enes@mbp-von-enes.localdomain>
2024-09-03 19:05:32 +0200

    final v.01

--- script.js
+++ script.js
@@ -5,31 +5,109 @@ const POKE_IMG = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprit
 
 let idCounter = 1;
 let toId = 41;
-let test
 
-async function renderPreviews() {
+async function loadMore() {
+    document.getElementById('loadMore_img').classList.add('pokeball-loading');
+    document.getElementById("loadMore_btn").disabled = true;
+    await renderPreviews(0);
+    document.getElementById('loadMore_img').classList.remove('pokeball-loading');
+    document.getElementById("loadMore_btn").disabled = false;
+    toId = toId + 20;
+}
+
+async function renderPreviews(count) {
+    if (count == 1) { idCounter = 1 }
     for (idCounter; idCounter < toId; idCounter++) {
-        let id = idCounter;
-        let pokeUrl = POKEAPI + id + '/';
+        let pokeUrl = POKEAPI + idCounter + '/';
         let pokemon = await fetchDataJson(pokeUrl);
         document.getElementById('previewContain').innerHTML += getPreviewTemp(pokemon);
-        
+
         renderTypes(pokemon);
-        setBackgroundColor(pokemon);
+        setBackgroundsColor(pokemon);
     }
-    toId = toId+20;
 }
 
-function showPokeCard(id) {
-    console.log(id);
+async function loadPokeCard(id) {
+    let pokeUrl = POKEAPI + id + '/';
+    let pokemon = await fetchDataJson(pokeUrl);
+    showPokeCard(pokemon);
+    checkButtons(id);
+
 }
 
-// async function pokeSpecies(id) {
-//     let speciesUrl = POKE_SPECIES + id + '/';
-//     let getSpeciesApi = await fetchDataJson(speciesUrl);
-//     let pokeColor = getSpeciesApi.color.name;
-//     return pokeColor;
-// }
+async function showPokeCard(pokemon) {
+    document.getElementById('modalContent').innerHTML = getModalTemp(pokemon);
+
+    renderTypesModal(pokemon);
+    setBackgroundColor(pokemon);
+
+    await renderAbout(pokemon);
+    renderStats(pokemon);
+    await renderAbility(pokemon)
+
+    setBackColors(pokemon);
+    setTextColors(pokemon)
+}
+
+function setBackColors(pokemon) {
+    let firstType = pokemon.types[0].type.name;
+    let typeColor = capitalizeFirstLetter(firstType);
+
+    let classElements = document.getElementsByClassName('type-background-color');
+
+    for (let i = 0; i < classElements.length; i++) {
+        const element = classElements[i];
+        element.style.backgroundColor = `${backgroundColors[typeColor]}`;
+    }
+}
+
+function setTextColors(pokemon) {
+    let firstType = pokemon.types[0].type.name;
+    let typeColor = capitalizeFirstLetter(firstType);
+
+    let classElements = document.getElementsByClassName('type-color');
+
+    for (let i = 0; i < classElements.length; i++) {
+        const element = classElements[i];
+        element.style.color = `${backgroundColors[typeColor]}`;
+    }
+}
+
+async function renderAbout(pokemon) {
+    const URL = POKE_SPECIES + pokemon.id;
+    const species = await fetchDataJson(URL);
+    const speciesArr = species.flavor_text_entries;
+
+    let flavorTextIndex = speciesArr.findIndex(i => i.language.name == 'en');
+    let flavorText = speciesArr[flavorTextIndex].flavor_text;
+
+    document.getElementById('pills-about').innerHTML += getModalAboutTemp(pokemon, flavorText);
+}
+
+function renderStats(pokemon) {
+    const statNames = ['HP', 'ATK', 'DEF', 'SATK', 'SDEF', 'SPD'];
+
+    for (let i = 0; i < statNames.length; i++) {
+        const statName = statNames[i];
+        const statValue = pokemon.stats[i].base_stat;
+        const statValuePricent = statValue / 255 * 100;
+
+        document.getElementById('pills-stats').innerHTML += getModalStatsTemp(statName, statValue, statValuePricent);
+    }
+}
+
+async function renderAbility(pokemon) {
+    const abilities = pokemon.abilities;
+
+    for (let i = 0; i < abilities.length; i++) {
+        const abilityName = abilities[i].ability.name;
+        const abilityURL = abilities[i].ability.url;
+        const abilityText = await fetchDataJson(abilityURL)
+        const text = abilityText.effect_entries[1].short_effect;
+
+        document.getElementById('pills-ability').innerHTML += getModalAbilitiesTemp(abilityName, text);
+    }
+}
 
 function getPokeImg(id) {
     let imgUrl = POKE_IMG + id + '.svg';
@@ -40,13 +118,43 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function nextPokemon(id) {
+    let nextId = id + 1;
+    if (nextId == idCounter) {
+        loadMore();
+    }
+    loadPokeCard(nextId);
+}
+
+function previousPokemon(id) {
+    let previousId = id - 1;
+    loadPokeCard(previousId);
+}
+
+function checkButtons(id) {
+    document.getElementById('previousButton').disabled = id == 1;
+}
+
+function renderTypesModal(pokemon) {
+    let typesArr = pokemon.types;
+
+    typesArr.forEach(slot => {
+        let type = slot.type.name;
+        let typeImgName = `Pokemon_Type_Icon_${capitalizeFirstLetter(type)}.svg`;
+        let typeColor = capitalizeFirstLetter(type);
+
+        document.getElementById(`pokeType_0`).innerHTML += getTypeTemp(typeImgName, type);
+        document.getElementById(`${type}`).style = `color: ${backgroundColors[typeColor]}`;
+    });
+}
+
 function renderTypes(pokemon) {
     let typesArr = pokemon.types;
 
     typesArr.forEach(slot => {
         let type = slot.type.name;
         let typeImgName = `Pokemon_Type_Icon_${capitalizeFirstLetter(type)}.svg`;
-    
+
         document.getElementById(`pokeType_${pokemon.id}`).innerHTML += getTypeTemp(typeImgName);
     });
 }
@@ -55,9 +163,15 @@ function setBackgroundColor(pokemon) {
     let firstType = pokemon.types[0].type.name;
     let typeColor = capitalizeFirstLetter(firstType);
 
-    document.getElementById(`backgroundColor_${pokemon.id}`).style = `background-color: ${backgroundColors[typeColor]}`;
+    document.getElementById(`backgroundColor_0`).style = `background-color: ${backgroundColors[typeColor]}`;
 }
 
+function setBackgroundsColor(pokemon) {
+    let firstType = pokemon.types[0].type.name;
+    let typeColor = capitalizeFirstLetter(firstType);
+
+    document.getElementById(`backgroundColor_${pokemon.id}`).style = `background-color: ${backgroundColors[typeColor]}`;
+}
 
 async function fetchDataJson(api) {
     let response = await fetch(api);
@@ -69,7 +183,41 @@ async function fetchDataJson(api) {
     }
 }
 
+async function searchPokemon() {
+    let searchWord = document.getElementById('searchInp').value;
+    if (searchWord.length > 2) {
+        let allPokemmonLink = await fetchDataJson(ALLPOKEMON);
+
+        let searchPokemons = allPokemmonLink.results
+            .filter(pokemon => pokemon['name'].includes(searchWord))
+            .map(pokemon => pokemon.name);
+
+        renderSearchPokemons(searchPokemons);
+    } else if (searchWord.length == 0) {
+        document.getElementById('previewContain').innerHTML = '';
+        await renderPreviews(1);
+        document.getElementById('loadMore_btn').classList.remove('d-none');
+    }
+}
+
+async function renderSearchPokemons(searchPokemons) {
+    document.getElementById('previewContain').innerHTML = '';
+    document.getElementById('loadMore_btn').classList.add('d-none');
+
+    for (let i = 0; i < searchPokemons.length; i++) {
+        const searchPokemon = searchPokemons[i];
+        let pokeUrl = POKEAPI + searchPokemon + '/';
+        let pokemon = await fetchDataJson(pokeUrl);
+
+        if (pokemon.id < 650) {
+            document.getElementById('previewContain').innerHTML += getPreviewTemp(pokemon);
+
+            renderTypes(pokemon);
+            setBackgroundsColor(pokemon);
+        }
+    };
+}
+
 function init() {
-    renderPreviews();
-    console.log('go!');
+    loadMore();
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -2,8 +2,8 @@ function getPreviewTemp(pokemon) {
     return /*html*/`
         <div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-2">
             <div class="square">
-                <div id="backgroundColor_${pokemon.id}" class="square-content pokemon-container poke-box-shadow rounded" onclick="showPokeCard(${pokemon.id})">
-                    <span class="pokemon-id"># ${pokemon.id}</span>
+                <div id="backgroundColor_${pokemon.id}" class="square-content pokemon-container poke-box-shadow rounded" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="loadPokeCard(${pokemon.id})">
+                    <span class="pokemon-id">#${String(pokemon.id).padStart(3, '0')}</span>
                     <img src="${getPokeImg(pokemon.id)}" class="rounded d-block poke-filter zoom pokemon-img">
                     <div class="pokemon-type" id="pokeType_${pokemon.id}"></div>
                     <span class="pokemon-name">${pokemon.name}</span>
@@ -13,8 +13,78 @@ function getPreviewTemp(pokemon) {
     `
 }
 
-function getTypeTemp(src) {
+function getTypeTemp(src, typeText = '') {
     return /*html*/`
         <img src="./assets/icons/type_icons/${src}">
+        <span id="${typeText}" class="type-text">${typeText}</span>
+    `
+}
+
+function getModalTemp(pokemon) {
+    return /*html*/`
+        <div class="card">
+
+            <div class="square">
+                <div id="backgroundColor_0" class="square-content modal-square-content pokemon-container poke-box-shadow rounded" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <span class="modal-id">#${String(pokemon.id).padStart(3, '0')}</span>
+                    <img src="${getPokeImg(pokemon.id)}" class="d-block modal-pokemon-img">
+                </div>
+
+            </div>
+            
+            <div class="modal-body-transition">
+                <span class="modal-pokemon-name">${pokemon.name}</span>
+                <div class="modal-pokemon-type" id="pokeType_0"></div>
+            </div>
+
+            <div class="card-body">
+
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link active" id="pills-about-tab" data-bs-toggle="pill" data-bs-target="#pills-about" type="button" role="tab" aria-controls="pills-about" aria-selected="true">About</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-stats-tab" data-bs-toggle="pill" data-bs-target="#pills-stats" type="button" role="tab" aria-controls="pills-stats" aria-selected="false">Stats</button>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <button class="nav-link" id="pills-ability-tab" data-bs-toggle="pill" data-bs-target="#pills-ability" type="button" role="tab" aria-controls="pills-ability" aria-selected="false">Ability</button>
+                </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-about" role="tabpanel" aria-labelledby="pills-about-tab" tabindex="0"></div>
+                <div class="tab-pane fade" id="pills-stats" role="tabpanel" aria-labelledby="pills-stats-tab" tabindex="0"></div>
+                <div class="tab-pane fade" id="pills-ability" role="tabpanel" aria-labelledby="pills-ability-tab" tabindex="0"></div>
+
+                <button id="previousButton" type="button" class="btn btn-dark left-button arrows" onclick="previousPokemon(${pokemon.id})"><img src="./assets/img/icons/left_arrow.png"></button>
+                <button type="button" class="btn btn-dark right-button arrows" onclick="nextPokemon(${pokemon.id})"><img src="./assets/img/icons/right_arrow.png"></button>
+            </div>
+        </div>
+    `
+}
+
+function getModalAboutTemp(pokemon, flavorText) {
+    return /*html*/`
+        <h5 class="type-color about-info"><img src="./assets/img/icons/height.png">${pokemon.height}</h5>
+        <h5 class="type-color about-info"><img src="./assets/img/icons/weight.png">${pokemon.weight}</h5>
+        <p>${flavorText}</p>
+    `
+}
+
+function getModalStatsTemp(statName, statValue, statValuePricent) {
+    return /*html*/`
+        <div class="progress-container">
+            <div class="progress-font">${statName}</div>
+            <div class="progress-font">${statValue}</div>
+            <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar type-background-color" style="width: ${statValuePricent}%"></div>
+            </div>
+        </div>
+    `
+}
+
+function getModalAbilitiesTemp(abilitiName, text) {
+    return /*html*/`
+        <h5 class="type-color">${abilitiName}</h5>
+        <p>${text}</p>
     `
 }
